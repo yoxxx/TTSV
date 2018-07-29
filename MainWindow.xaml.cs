27965a55e7f93fa457c64485a08ec332231dfb89
yoxxx <40835429+yoxxx@users.noreply.github.com>
2018-07-30 00:25:02 +0900

    Ver.0.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,7 +7,7 @@
  * TTSV is free software; you can redistribute it and/or
  * modify it under the terms of the BSD License.
  *
- * TI2VSQX is distributed in the hope that it will be useful,
+ * TTSV is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
